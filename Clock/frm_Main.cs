480c21d4b67fde8a1e0c8409febe7d259ae532da
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-10-22 12:56:19 +0100

    Creating the main layout

--- Clock/frm_Main.cs
+++ Clock/frm_Main.cs
@@ -1,8 +1,8 @@
 namespace Clock
 {
-    public partial class Form1 : Form
+    public partial class frm_Main : Form
     {
-        public Form1()
+        public frm_Main()
         {
             InitializeComponent();
         }
