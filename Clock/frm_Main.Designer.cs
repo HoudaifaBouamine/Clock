7e9868134dd23674f965f1f550572855869e9975
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-10-23 14:02:09 +0100

    Creating Cust layout

--- Clock/frm_Main.Designer.cs
+++ Clock/frm_Main.Designer.cs
@@ -28,12 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            pnl_SmallSideMenu = new Panel();
+            pnl_SmallSideMenuHeader = new Panel();
+            pnl_SideMenu = new Panel();
+            pnl_SideMenuHeader = new Panel();
+            pnl_MainContant = new Panel();
+            pnl_MainContantHeader = new Panel();
+            btn_Minimize = new Button();
+            btn_Maximize = new Button();
+            btn_Close = new Button();
+            pnl_SmallSideMenu.SuspendLayout();
+            pnl_SideMenu.SuspendLayout();
+            pnl_MainContant.SuspendLayout();
+            pnl_MainContantHeader.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnl_SmallSideMenu
+            // 
+            pnl_SmallSideMenu.BackColor = Color.FromArgb(32, 32, 32);
+            pnl_SmallSideMenu.Controls.Add(pnl_SmallSideMenuHeader);
+            pnl_SmallSideMenu.Dock = DockStyle.Left;
+            pnl_SmallSideMenu.Location = new Point(4, 4);
+            pnl_SmallSideMenu.Name = "pnl_SmallSideMenu";
+            pnl_SmallSideMenu.Size = new Size(10, 625);
+            pnl_SmallSideMenu.TabIndex = 0;
+            // 
+            // pnl_SmallSideMenuHeader
+            // 
+            pnl_SmallSideMenuHeader.Dock = DockStyle.Top;
+            pnl_SmallSideMenuHeader.Location = new Point(0, 0);
+            pnl_SmallSideMenuHeader.Name = "pnl_SmallSideMenuHeader";
+            pnl_SmallSideMenuHeader.Size = new Size(10, 45);
+            pnl_SmallSideMenuHeader.TabIndex = 0;
+            pnl_SmallSideMenuHeader.MouseDown += pnl_MainContantHeader_MouseDown;
+            // 
+            // pnl_SideMenu
+            // 
+            pnl_SideMenu.BackColor = Color.FromArgb(32, 32, 32);
+            pnl_SideMenu.Controls.Add(pnl_SideMenuHeader);
+            pnl_SideMenu.Dock = DockStyle.Left;
+            pnl_SideMenu.Location = new Point(14, 4);
+            pnl_SideMenu.Name = "pnl_SideMenu";
+            pnl_SideMenu.Size = new Size(172, 625);
+            pnl_SideMenu.TabIndex = 1;
+            // 
+            // pnl_SideMenuHeader
+            // 
+            pnl_SideMenuHeader.Dock = DockStyle.Top;
+            pnl_SideMenuHeader.Location = new Point(0, 0);
+            pnl_SideMenuHeader.Name = "pnl_SideMenuHeader";
+            pnl_SideMenuHeader.Size = new Size(172, 45);
+            pnl_SideMenuHeader.TabIndex = 1;
+            pnl_SideMenuHeader.MouseDown += pnl_MainContantHeader_MouseDown;
+            // 
+            // pnl_MainContant
+            // 
+            pnl_MainContant.BackColor = Color.FromArgb(39, 39, 39);
+            pnl_MainContant.Controls.Add(pnl_MainContantHeader);
+            pnl_MainContant.Dock = DockStyle.Fill;
+            pnl_MainContant.Location = new Point(186, 4);
+            pnl_MainContant.Name = "pnl_MainContant";
+            pnl_MainContant.Size = new Size(810, 625);
+            pnl_MainContant.TabIndex = 2;
+            // 
+            // pnl_MainContantHeader
+            // 
+            pnl_MainContantHeader.Controls.Add(btn_Minimize);
+            pnl_MainContantHeader.Controls.Add(btn_Maximize);
+            pnl_MainContantHeader.Controls.Add(btn_Close);
+            pnl_MainContantHeader.Dock = DockStyle.Top;
+            pnl_MainContantHeader.Location = new Point(0, 0);
+            pnl_MainContantHeader.Name = "pnl_MainContantHeader";
+            pnl_MainContantHeader.Size = new Size(810, 45);
+            pnl_MainContantHeader.TabIndex = 1;
+            pnl_MainContantHeader.MouseDown += pnl_MainContantHeader_MouseDown;
+            // 
+            // btn_Minimize
+            // 
+            btn_Minimize.BackColor = Color.FromArgb(39, 39, 39);
+            btn_Minimize.BackgroundImage = Properties.Resources.img_Minimize1;
+            btn_Minimize.BackgroundImageLayout = ImageLayout.Center;
+            btn_Minimize.Dock = DockStyle.Right;
+            btn_Minimize.FlatAppearance.BorderSize = 0;
+            btn_Minimize.FlatAppearance.MouseDownBackColor = Color.FromArgb(207, 207, 207);
+            btn_Minimize.FlatAppearance.MouseOverBackColor = Color.FromArgb(255, 255, 255);
+            btn_Minimize.FlatStyle = FlatStyle.Flat;
+            btn_Minimize.ForeColor = Color.FromArgb(255, 255, 255);
+            btn_Minimize.Location = new Point(600, 0);
+            btn_Minimize.Name = "btn_Minimize";
+            btn_Minimize.Size = new Size(70, 45);
+            btn_Minimize.TabIndex = 2;
+            btn_Minimize.UseVisualStyleBackColor = false;
+            btn_Minimize.MouseEnter += btn_Minimize_MouseEnter;
+            btn_Minimize.MouseLeave += btn_Minimize_MouseLeave;
+            // 
+            // btn_Maximize
+            // 
+            btn_Maximize.BackColor = Color.FromArgb(39, 39, 39);
+            btn_Maximize.BackgroundImage = Properties.Resources.img_Maximize1;
+            btn_Maximize.BackgroundImageLayout = ImageLayout.Center;
+            btn_Maximize.Dock = DockStyle.Right;
+            btn_Maximize.FlatAppearance.BorderSize = 0;
+            btn_Maximize.FlatAppearance.MouseDownBackColor = Color.FromArgb(207, 207, 207);
+            btn_Maximize.FlatAppearance.MouseOverBackColor = Color.FromArgb(255, 255, 255);
+            btn_Maximize.FlatStyle = FlatStyle.Flat;
+            btn_Maximize.ForeColor = Color.FromArgb(255, 255, 255);
+            btn_Maximize.Location = new Point(670, 0);
+            btn_Maximize.Name = "btn_Maximize";
+            btn_Maximize.Size = new Size(70, 45);
+            btn_Maximize.TabIndex = 1;
+            btn_Maximize.UseVisualStyleBackColor = false;
+            btn_Maximize.Click += btn_Maximize_Click;
+            btn_Maximize.MouseEnter += btn_Maximize_MouseEnter;
+            btn_Maximize.MouseLeave += btn_Maximize_MouseLeave;
+            // 
+            // btn_Close
+            // 
+            btn_Close.BackgroundImage = Properties.Resources.img_Close1;
+            btn_Close.BackgroundImageLayout = ImageLayout.Center;
+            btn_Close.Dock = DockStyle.Right;
+            btn_Close.FlatAppearance.BorderSize = 0;
+            btn_Close.FlatAppearance.MouseDownBackColor = Color.FromArgb(232, 17, 35);
+            btn_Close.FlatAppearance.MouseOverBackColor = Color.FromArgb(232, 17, 35);
+            btn_Close.FlatStyle = FlatStyle.Flat;
+            btn_Close.Location = new Point(740, 0);
+            btn_Close.Name = "btn_Close";
+            btn_Close.Size = new Size(70, 45);
+            btn_Close.TabIndex = 0;
+            btn_Close.UseVisualStyleBackColor = true;
+            btn_Close.Click += btn_Close_Click;
+            btn_Close.MouseEnter += btn_Close_MouseEnter;
+            btn_Close.MouseLeave += btn_Close_MouseLeave;
+            // 
+            // frm_Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1000, 633);
+            Controls.Add(pnl_MainContant);
+            Controls.Add(pnl_SideMenu);
+            Controls.Add(pnl_SmallSideMenu);
+            Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "frm_Main";
+            Padding = new Padding(4);
             Text = "Form1";
+            pnl_SmallSideMenu.ResumeLayout(false);
+            pnl_SideMenu.ResumeLayout(false);
+            pnl_MainContant.ResumeLayout(false);
+            pnl_MainContantHeader.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel pnl_SmallSideMenu;
+        private Panel pnl_SmallSideMenuHeader;
+        private Panel pnl_SideMenu;
+        private Panel pnl_SideMenuHeader;
+        private Panel pnl_MainContant;
+        private Panel pnl_MainContantHeader;
+        private Button btn_Maximize;
+        private Button btn_Close;
+        private Button btn_Minimize;
     }
 }
--- Clock/frm_Main.cs
+++ Clock/frm_Main.cs
@@ -1,10 +1,167 @@
+
+using Clock.Properties;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using System.Windows;
+
 namespace Clock
 {
     public partial class frm_Main : Form
     {
+        //public frm_Main()
+        //{
+        //    InitializeComponent();
+        //}
         public frm_Main()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None; // no borders
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw, true); // this is to avoid visual artifacts
+
+
         }
+
+        #region Resize Window
+        protected override void OnPaint(PaintEventArgs e) // you can safely omit this method if you want
+        {
+            Brush brush = new SolidBrush(Color.FromArgb(255, 255, 255));
+            e.Graphics.FillRectangle(new SolidBrush((clsSettings.MainContantBackGroundColor)), Top);
+            e.Graphics.FillRectangle(new SolidBrush((clsSettings.SideMenuBackGroundColor)), Left);
+            e.Graphics.FillRectangle(new SolidBrush((clsSettings.MainContantBackGroundColor)), Right);
+            e.Graphics.FillRectangle(new SolidBrush((clsSettings.MainContantBackGroundColor)), Bottom);
+        }
+
+        private const int
+            HTLEFT = 10,
+            HTRIGHT = 11,
+            HTTOP = 12,
+            HTTOPLEFT = 13,
+            HTTOPRIGHT = 14,
+            HTBOTTOM = 15,
+            HTBOTTOMLEFT = 16,
+            HTBOTTOMRIGHT = 17;
+
+        const int Thikness = 4; // you can rename this variable if you like
+
+        Rectangle Top { get { return new Rectangle(0, 0, this.ClientSize.Width, Thikness); } }
+        Rectangle Left { get { return new Rectangle(0, 0, Thikness, this.ClientSize.Height); } }
+        Rectangle Bottom { get { return new Rectangle(0, this.ClientSize.Height - Thikness, this.ClientSize.Width, Thikness); } }
+        Rectangle Right { get { return new Rectangle(this.ClientSize.Width - Thikness, 0, Thikness, this.ClientSize.Height); } }
+
+        Rectangle TopLeft { get { return new Rectangle(0, 0, Thikness, Thikness); } }
+        Rectangle TopRight { get { return new Rectangle(this.ClientSize.Width - Thikness, 0, Thikness, Thikness); } }
+        Rectangle BottomLeft { get { return new Rectangle(0, this.ClientSize.Height - Thikness, Thikness, Thikness); } }
+        Rectangle BottomRight { get { return new Rectangle(this.ClientSize.Width - Thikness, this.ClientSize.Height - Thikness, Thikness, Thikness); } }
+
+
+        protected override void WndProc(ref Message message)
+        {
+            base.WndProc(ref message);
+
+            if(WindowState == FormWindowState.Maximized) { return; }
+
+            if (message.Msg == 0x84) // WM_NCHITTEST
+            {
+                var cursor = this.PointToClient(Cursor.Position);
+
+                if (TopLeft.Contains(cursor)) message.Result = (IntPtr)HTTOPLEFT;
+                else if (TopRight.Contains(cursor)) message.Result = (IntPtr)HTTOPRIGHT;
+                else if (BottomLeft.Contains(cursor)) message.Result = (IntPtr)HTBOTTOMLEFT;
+                else if (BottomRight.Contains(cursor)) message.Result = (IntPtr)HTBOTTOMRIGHT;
+
+                else if (Top.Contains(cursor)) message.Result = (IntPtr)HTTOP;
+                else if (Left.Contains(cursor)) message.Result = (IntPtr)HTLEFT;
+                else if (Right.Contains(cursor)) message.Result = (IntPtr)HTRIGHT;
+                else if (Bottom.Contains(cursor)) message.Result = (IntPtr)HTBOTTOM;
+
+            }
+        }
+
+        #endregion
+
+        #region Move Window
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+
+        private void pnl_MainContantHeader_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        #endregion
+
+        private void btn_Close_MouseHover(object sender, EventArgs e)
+        {
+        }
+
+        private void btn_Maximize_MouseEnter(object sender, EventArgs e)
+        {
+            btn_Maximize.BackgroundImage = Resources.img_Maximize2;
+        }
+
+        private void btn_Maximize_MouseLeave(object sender, EventArgs e)
+        {
+            btn_Maximize.BackgroundImage = Resources.img_Maximize1;
+
+        }
+
+        private void btn_Close_MouseEnter(object sender, EventArgs e)
+        {
+            btn_Close.BackgroundImage = Resources.img_Close2;
+
+        }
+
+        private void btn_Close_MouseLeave(object sender, EventArgs e)
+        {
+            btn_Close.BackgroundImage = Resources.img_Close1;
+
+        }
+
+        private void btn_Minimize_MouseEnter(object sender, EventArgs e)
+        {
+            btn_Minimize.BackgroundImage = Resources.img_Minimize2;
+        }
+
+        private void btn_Minimize_MouseLeave(object sender, EventArgs e)
+        {
+            btn_Minimize.BackgroundImage = Resources.img_Minimize1;
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Maximize_Click(object sender, EventArgs e)
+        {
+
+
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+        }
+
+
     }
+
+
+
 }
